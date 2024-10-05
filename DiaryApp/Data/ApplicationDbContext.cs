c4e69ff258e74f6f8698bcfd62ac2f8dae6e0cef
harrytimbog <harriemannie@gmail.com>
2024-10-05 19:25:46 +0100

    Seeding our Diary Entry Table

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,35 @@ namespace DiaryApp.Data
         }
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry { 
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Dede!",
+                    Created = DateTime.Now
+                },
+
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went to Codonas",
+                    Content = "Went to Codonas with Okechukwu",
+                    Created = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Coding",
+                    Content = "Went to Coding with CoBots",
+                    Created = DateTime.Now
+                }
+
+            );
+        }
     }
 }
---
+++ DiaryApp/Migrations/20241005182215_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Dede!", new DateTime(2024, 10, 5, 19, 22, 14, 605, DateTimeKind.Local).AddTicks(7489), "Went Hiking" },
+                    { 2, "Went to Codonas with Okechukwu", new DateTime(2024, 10, 5, 19, 22, 14, 605, DateTimeKind.Local).AddTicks(7494), "Went to Codonas" },
+                    { 3, "Went to Coding with CoBots", new DateTime(2024, 10, 5, 19, 22, 14, 605, DateTimeKind.Local).AddTicks(7499), "Went Coding" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
