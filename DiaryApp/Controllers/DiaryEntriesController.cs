40f6f4ae00f1054af3b25cdd5c452026c3afd6ca
harrytimbog <harriemannie@gmail.com>
2024-10-06 17:35:18 +0100

    Read data ad Display it in our web app

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        // 
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            // Get all diary entries from the database
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
+
+            // Return the list of diary entries to the view
+            return View(objDiaryEntryList);
         }
     }
 }
